334a82875a799b557b183ec5442359310aa003c0
sunshine09101 <sunshine09101@gmail.com>
2022-03-30 06:34:19 +0200

    owner wallet address changed

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const infuraKey = 'b87b96a22d544319809fa30f8405f44d' //process.env.REACT_
 
 export const collectionAddress = '0xB125Cd41f16f8E95d7CE2fE9Eca903A01b0ee1a8';// Collection of NFTS
 export const factoryAddress = '0x8848C072C2305Be82c33E939485db6cC56cE7ec0';// Deployed Contract for staking
-export const createPoolOwner = '0x456101a94036204D3F2E15b0FdAC99e6BD5E98af';// Dev Wallet who will fund the pool.
+export const createPoolOwner = '0xc368918959611A2f2A793060a127Aa1054Fe82B0';// Dev Wallet who will fund the pool.
 export const paginationLimit = 8;
 
 export const infuraAddress = `https://mainnet.infura.io/v3/${infuraKey}`
