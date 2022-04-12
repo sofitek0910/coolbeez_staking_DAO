b7e7dcd8eb4e0cf82d7080b38e463baddf91f5c6
unknown <sunshine09101@gmail.com>
2022-04-12 10:24:24 -0700

    truffle config finished

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,6 @@
+const coolbeez = artifacts.require("coolbeez");
+
+module.exports = function (deployer) {
+  deployer.deploy(coolbeez);
+};
+                               
---
+++ truffle-config.js
@@ -0,0 +1,19 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const MNEMONIC = 'jazz polar shock swamp pumpkin gossip type skill luggage relief unhappy boost';
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/YOUR_API_KEY")
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
+    }
+  }
+};
